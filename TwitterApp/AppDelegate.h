6308954acf7cd82569d4085c34293b8fa609600d
Hegyi <hajhegyi@hegyis-mbp.misys.global.ad>
2016-01-11 09:59:41 +0100

    Initial commit

---
+++ TwitterApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterApp
+//
+//  Created by Hegyi Hajnalka on 11/01/16.
+//  Copyright © 2016 Hegyi Hajnalka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterApp
+//
+//  Created by Hegyi Hajnalka on 11/01/16.
+//  Copyright © 2016 Hegyi Hajnalka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
